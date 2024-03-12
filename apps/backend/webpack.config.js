f0349893650d402c386764f5c8338e881aa4ee12
quangbm00 <quangbm@missingcorner.com>
2024-03-12 21:55:43 +0700

    feat: init

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/remitano'),
+    path: join(__dirname, '../../dist/apps/backend'),
   },
   plugins: [
     new NxWebpackPlugin({
